81047338d2dcef03f0c79aaa78dac1684efa8e03
Jake Cornelius <jakecornds@gmail.com>
2026-02-24 18:35:34 -0600

    Refactor conversion and buffer splitting logic

--- code/convertToBaseUnit.c
+++ code/convertToBaseUnit.c
@@ -1,48 +1,63 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-double convertToBaseUnit(char buffer[])
-{
-    char valueString[16];
-    double value = 0;
-    int stringCount = 0;
-    for (int c = 0; c < strlen(buffer); c++){
-        
-        if (buffer[c] >= '0' && buffer[c] <= '9' || buffer[c] == '.')
-        {
-            valueString[c] = buffer[c]; 
-            stringCount++;
-        }
-    }
-    valueString[stringCount] = '\0';
-    value = atof(valueString);
-    char *remainingString = buffer + stringCount;
-    if (remainingString[0] == 'i') 
-    {
-        value *= 25.4;
-    }
-    //printf("%.3lf\n", value);
+
+void convert(char* input, char* output){
     
-    return value;
+    if (strchr(input, 'i') != NULL){
+        double coef = atof(input);
+        coef *= 25.4;
+        sprintf(output, "%g", coef);
+    } else {
+        double coef = atof(input);
+        sprintf(output, "%g", coef);
+    }
+    
 }
 
-void float_to_string(float num, char *buffer, int buffer_size) {
-    // %.2f limits the output to 2 decimal places. 
-    // Use %f for default precision (usually 6).
-    snprintf(buffer, buffer_size, "%.3f", num);
+
+void splitBuffer(char* input, char* output)
+{
+    char term[32] = {0};
+    char convertedTerm[32] = {0};
+    int termIdx = 0;
+    output[0] = '\0';
+    
+    for (int i = 0; i <= strlen(input); i++)
+    {
+        char c = input[i];
+        
+        if (c == '+'|| c == '-'|| c == '*'|| c == '/' || c == '\0') {
+            
+            if (termIdx > 0) {
+                term[termIdx] = '\0';
+                convert(term, convertedTerm);
+                strcat(output, convertedTerm);
+                termIdx = 0;
+            }
+            
+            if (c != '\0'){
+                int len = strlen(output);
+                output[len] = c;
+                output[len + 1] = '\0';
+            }
+        } else if (c != ' ') {
+            term[termIdx++] = c;
+        }
+    }
+    
 }
 
 int main(){
     
-    char input_buffer[32];
-    char convertedString[32];
-    convertedString[0] = '\0';
-    float_to_string(convertToBaseUnit("1in"), input_buffer, sizeof(input_buffer));
-    strcat(convertedString, input_buffer);
-    float_to_string(convertToBaseUnit("12mm"), input_buffer, sizeof(input_buffer));
-    strcat(convertedString, input_buffer);
-    printf("%s", convertedString);
+    char buffer[] = "1in+25.4mm+13in*55mm-15";
+    char convertedBuffer[100] = {0};
+    
+    splitBuffer(buffer, convertedBuffer);
+    
+    printf("Orginal Buffer: %s\n", buffer);
+    printf("convertedBuffer: %s\n", convertedBuffer);
     
     return 0;
 }
