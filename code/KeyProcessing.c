87f6dbd565e42e3caf2712351a348b6a79f9ba95
Jake Cornelius <jakecornds@gmail.com>
2026-02-25 20:27:12 -0600

    Refactor key processing to use calculator_t structure
    Refactor key processing functions to use a calculator_t structure for state management. Update function signatures and implementations to accommodate the new structure.

--- code/KeyProcessing.c
+++ code/KeyProcessing.c
@@ -6,94 +6,158 @@
 
 #define MAX_INPUT 64
 
-char input_buffer[MAX_INPUT];
-uint8_t input_length = 0;
-bool decimal_used = false;
+typedef struct {
+	char input_buffer[MAX_INPUT];
+	uint8_t input_length;
+} calculator_t;
+
 
 typedef enum {
-    KEY_MM,
-    KEY_IN,
-    KEY_TOMM,
-    KEY_TOIN,
-    KEY_7,
-    KEY_8,
-    KEY_9,
-    KEY_DIVIDE,
-    KEY_4,
-    KEY_5,
-    KEY_6,
-    KEY_MULTIPLY,
-    KEY_1,
-    KEY_2,
-    KEY_3,
-    KEY_SUBTRACT,
-    KEY_0,
-    KEY_DECIMAL,
-    KEY_C,
-    KEY_ADD
+	TO_MM,
+	TO_IN
+} unit_t;
+
+typedef enum {
+	KEY_MM,
+	KEY_IN,
+	KEY_TOMM,
+	KEY_TOIN,
+	KEY_7,
+	KEY_8,
+	KEY_9,
+	KEY_DIVIDE,
+	KEY_4,
+	KEY_5,
+	KEY_6,
+	KEY_MULTIPLY,
+	KEY_1,
+	KEY_2,
+	KEY_3,
+	KEY_SUBTRACT,
+	KEY_0,
+	KEY_DECIMAL,
+	KEY_C,
+	KEY_ADD
 } Key_t;
 
-void appendChar(char c)
+void appendChar(const char c, calculator_t *calc)
 {
-    if (input_length < MAX_INPUT - 1) {
-        input_buffer[input_length++] = c;
-        input_buffer[input_length] = '\0';
-    }
+	if (calc->input_length < MAX_INPUT - 1) {
+		calc->input_buffer[calc->input_length++] = c;
+		calc->input_buffer[calc->input_length] = '\0';
+	}
 }
 
-void appendString(char *unit){
-    if (input_length < MAX_INPUT - 2){
-        strcat(input_buffer, unit);
-    }
+void appendString(const char *unit, calculator_t *calc) {
+	if (calc->input_length < MAX_INPUT - 2)
+	{
+		memcpy(&calc->input_buffer[calc->input_length], unit, 2);
+		calc->input_length += 2;
+		calc->input_buffer[calc->input_length] = '\0';
+	}
 }
 
-void calculation(){
-    printf(input_buffer);
+void clearBuffer(calculator_t *calc)
+{
+	calc->input_length = 0;
+	calc->input_buffer[0] = '\0';
 }
 
-void processKey(Key_t key){
-    switch(key)
-    {
-        case KEY_0: appendChar('0'); break;
-        case KEY_1: appendChar('1'); break;
-        case KEY_2: appendChar('2'); break;
-        case KEY_3: appendChar('3'); break;
-        case KEY_4: appendChar('4'); break;
-        case KEY_5: appendChar('5'); break;
-        case KEY_6: appendChar('6'); break;
-        case KEY_7: appendChar('7'); break;
-        case KEY_8: appendChar('8'); break;
-        case KEY_9: appendChar('9'); break;
-        
-        case KEY_DECIMAL: appendChar('.'); break;
-        
-        case KEY_ADD: appendChar('+'); break;
-        case KEY_SUBTRACT: appendChar('-'); break;
-        case KEY_MULTIPLY: appendChar('*'); break;
-        case KEY_DIVIDE: appendChar('/'); break;
-        
-        case KEY_MM: appendString("mm"); break;
-        case KEY_IN: appendString("in"); break;
-        
-        case KEY_TOIN: calculation(); break;
-        case KEY_TOMM: calculation(); break;
-        
-        default: break;
-        
-    }
+void calculation(calculator_t *calc, unit_t unit) {
+	printf("%s\n", calc->input_buffer);
+	printf("%d", unit);
+}
+
+char keyToChar(Key_t key)
+{
+	switch (key)
+	{
+	case KEY_0:
+		return '0';
+	case KEY_1:
+		return '1';
+	case KEY_2:
+		return '2';
+	case KEY_3:
+		return '3';
+	case KEY_4:
+		return '4';
+	case KEY_5:
+		return '5';
+	case KEY_6:
+		return '6';
+	case KEY_7:
+		return '7';
+	case KEY_8:
+		return '8';
+	case KEY_9:
+		return '9';
+	default:
+		return '\0';
+	}
+}
+
+void processKey(Key_t key, calculator_t *calc) {
+
+	char digit = keyToChar(key);
+	if (digit) {
+		appendChar(digit, calc);
+		return;
+	}
+
+	switch(key)
+	{
+	case KEY_DECIMAL:
+		appendChar('.', calc);
+		break;
+	case KEY_ADD:
+		appendChar('+', calc);
+		break;
+	case KEY_SUBTRACT:
+		appendChar('-', calc);
+		break;
+	case KEY_MULTIPLY:
+		appendChar('*', calc);
+		break;
+	case KEY_DIVIDE:
+		appendChar('/', calc);
+		break;
+
+	case KEY_MM:
+		appendString("mm", calc);
+		break;
+	case KEY_IN:
+		appendString("in", calc);
+		break;
+	case KEY_C:
+		clearBuffer(calc);
+		break;
+	case KEY_TOIN:
+		calculation(calc, TO_IN);
+		break;
+	case KEY_TOMM:
+		calculation(calc, TO_MM);
+		break;
+	default:
+		break;
+	}
 }
 
 int main()
 {
-    processKey(KEY_3);
-    processKey(KEY_DIVIDE);
-    processKey(KEY_4);
-    processKey(KEY_IN);
-    processKey(KEY_ADD);
-    processKey(KEY_1);
-    processKey(KEY_2);
-    processKey(KEY_MM);
-    processKey(KEY_TOIN);
-    
-    return 0;
+
+	calculator_t calc = {0};
+
+	processKey(KEY_3, &calc);
+	processKey(KEY_DIVIDE, &calc);
+	processKey(KEY_4, &calc);
+	processKey(KEY_IN, &calc);
+	processKey(KEY_ADD, &calc);
+	processKey(KEY_1, &calc);
+	processKey(KEY_2, &calc);
+	processKey(KEY_MM, &calc);
+
+	processKey(KEY_TOMM, &calc);
+
+	return 0;
 }
